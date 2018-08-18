b5903bc5d9accee110eb6e3eb406eba20971b226
Regallia <38038568+Regallia@users.noreply.github.com>
2018-08-18 19:43:09 +0900

    tes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+
+window.onload = function(){$("#showPassword").hide();}
+
+$("#txtPassword").on('change',function() {  
+    if($("#txtPassword").val())
+    {
+      $("#showPassword").show();
+    }
+    else
+    {
+      $("#showPassword").hide();
+    }
+});
+
+$(".reveal").on('click',function() {
+    var $pwd = $("#txtPassword");
+    if ($pwd.attr('type') === 'password') 
+    {
+        $pwd.attr('type', 'text');
+    } 
+    else 
+    {
+        $pwd.attr('type', 'password');
+    }
+});
+
+
 $('#menu-action').click(function() {
   $('.sidebar').toggleClass('active');
   $('.main').toggleClass('active');
